d020364a3ea4bf4263d2d7201335c0f86fead3eb
Ignacio Aranda <87491116+elnacho02@users.noreply.github.com>
2021-11-15 05:19:26 -0300

    asdasd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ function Navbar({isLog, logVerif, setIsLog}) {
                 </div>
             </div>
             <div className={s.user}>
-                {isLog === false && (
+                {(isLog === false || !isLog) && (
                     <NavLink to="/login" className={s.login}>
                         <i class="fas fa-sign-in-alt"></i>
                         Sign In
